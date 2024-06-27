942ce2df2a0278104437de6f00da5be23cd93554
Shambhavi Sharma <shambhavi.sharma2612@gmail.com>
2024-06-27 12:32:34 +0530

    initial commit

---
+++ StrategyPattern/PaymentContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public class PaymentContext
+    {
+        private IPaymentStrategy _paymentStrategy;
+
+        public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+
+        public void Pay(int amount)
+        {
+            _paymentStrategy.Pay(amount);
+        }
+    }
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PaymentContext paymentContext = new PaymentContext();
+
+
+            paymentContext.SetPaymentStrategy(new CreditCardPayment());
+            paymentContext.Pay(100);
+
+
+            paymentContext.SetPaymentStrategy(new PayPalPayment());
+            paymentContext.Pay(200);
+
+            Console.ReadLine();
+        }
+    }
+}
